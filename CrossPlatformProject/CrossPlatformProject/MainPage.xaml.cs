b1b0f46aa184ffb0fb67f490f5c1a3c6abfd25f1
Daniel Nelga <g00464658@atu.ie>
2025-11-21 21:07:39 +0000

    cleaned repo and removed .vs folder

--- CrossPlatformProject/CrossPlatformProject/MainPage.xaml.cs
+++ CrossPlatformProject/CrossPlatformProject/MainPage.xaml.cs
@@ -1,8 +1,12 @@
-﻿namespace CrossPlatformProject
+﻿using System.Text.Json;
+namespace CrossPlatformProject
 {
     public partial class MainPage : ContentPage
     {
-   
+        string jsonFileGithub = "https://raw.githubusercontent.com/DonH-ITS/jsonfiles/refs/heads/main/moviesemoji.json";
+        string localCache = Path.Combine(FileSystem.AppDataDirectory, "movies.json");
+
+        List<Movie> allMovies = new List<Movie>();
 
         public MainPage()
         {
---
+++ CrossPlatformProject/CrossPlatformProject/movie.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace CrossPlatformProject
+{
+    public partial class movie
+    {
+        //Means to search
+        public string Title { get; set; }
+        public List<string> Genre { get; set; }
+        public string Emoji { get; set; }
+    }
+}
