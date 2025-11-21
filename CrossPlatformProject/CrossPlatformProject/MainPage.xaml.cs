26c12f660d0b8499dbd87898c6a5e5ad104ffb51
Daniel Nelga <g00464658@atu.ie>
2025-11-21 21:21:13 +0000

    Removed nested .vs folder

--- CrossPlatformProject/CrossPlatformProject/MainPage.xaml.cs
+++ CrossPlatformProject/CrossPlatformProject/MainPage.xaml.cs
@@ -6,6 +6,7 @@ namespace CrossPlatformProject
         string jsonFileGithub = "https://raw.githubusercontent.com/DonH-ITS/jsonfiles/refs/heads/main/moviesemoji.json";
         string localCache = Path.Combine(FileSystem.AppDataDirectory, "movies.json");
 
+        //creating the list and then will store the movie objects into this
         List<Movie> allMovies = new List<Movie>();
 
         public MainPage()
