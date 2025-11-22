abff32abde2ddb54b351dd4a8447e3e49960265e
Daniel Nelga <g00464658@atu.ie>
2025-11-22 18:10:52 +0000

    poxui

--- CrossPlatformProject/CrossPlatformProject/MainPage.xaml.cs
+++ CrossPlatformProject/CrossPlatformProject/MainPage.xaml.cs
@@ -3,10 +3,13 @@ namespace CrossPlatformProject
 {
     public partial class MainPage : ContentPage
     {
+        //using the json API can getting movies+movie objects from here
         string jsonFileGithub = "https://raw.githubusercontent.com/DonH-ITS/jsonfiles/refs/heads/main/moviesemoji.json";
+
+        //storing in local cache here
         string localCache = Path.Combine(FileSystem.AppDataDirectory, "movies.json");
 
-        //creating the list and then will store the movie objects into this
+        //creating the list and then will store the movie objects into this list
         List<Movie> allMovies = new List<Movie>();
 
         public MainPage()
